7ac0ec890983d295616eef22daeba25f159672b4
shankroy <shank.roy14@gmail.com>
2020-02-24 11:07:53 +0530

    contact model added

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def bloghome(request):
-    return HttpResponse('This is bloghome.We will keep all the posts here')
+    return render(request,'blog\\bloghome.html')
+    #return HttpResponse('This is bloghome.We will keep all the posts here')
     
 def blogpost(request,slug):
-    return HttpResponse(f'This is blog :{slug}')
+    return render(request,'blog\\blogpost.html')
+    #return HttpResponse(f'This is blog :{slug}')
--- home/views.py
+++ home/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('This is home')
+    return render(request,'home\home.html')
+    #return HttpResponse('This is home')
     
 def contact(request):
-    return HttpResponse('This is contact')
+    return render(request,'home\contact.html')
+    #return HttpResponse('This is contact')
 
 def about(request):
-    return HttpResponse('This is about')
+    return render(request,'home\\about.html')
+    #return HttpResponse('This is about')
